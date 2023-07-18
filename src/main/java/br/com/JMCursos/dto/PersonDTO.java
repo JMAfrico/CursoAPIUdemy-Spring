292a818c2e0681c063c29a532f3fad01023390b1
JMAfrico <joao_marcossilva@hotmail.com>
2023-07-18 17:59:53 -0300

    add PersonDTO

---
+++ src/main/java/br/com/JMCursos/dto/PersonDTO.java
@@ -0,0 +1,100 @@
+package br.com.JMCursos.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import br.com.JMCursos.models.Person;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//Representa uma tabela no banco de dados = "@Entity"
+@Entity
+@Table(name="person")
+public class PersonDTO implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	private String firstName;
+	
+	private String adress;
+	
+	public PersonDTO() {
+		
+	}
+	
+	public PersonDTO(Person person) {
+		this.id = person.getId();
+		this.firstName = person.getFirstName();
+		this.adress = person.getAdress();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adress == null) ? 0 : adress.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonDTO other = (PersonDTO) obj;
+		if (adress == null) {
+			if (other.adress != null)
+				return false;
+		} else if (!adress.equals(other.adress))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
